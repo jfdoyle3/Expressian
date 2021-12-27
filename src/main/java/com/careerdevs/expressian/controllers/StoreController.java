406ac1e749cb61a1fe46e5f42ff44bc0693e0701
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2021-12-27 00:39:18 -0500

    Store connected with Vehicle.

--- src/main/java/com/careerdevs/expressian/controllers/StoreController.java
+++ src/main/java/com/careerdevs/expressian/controllers/StoreController.java
@@ -1,50 +1,50 @@
-//package com.careerdevs.expressian.controllers;
-//
-//
-//import com.careerdevs.expressian.entities.Store;
-//import com.careerdevs.expressian.repositories.VehicleRepository;
-//import com.careerdevs.expressian.repositories.StoreRepository;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.util.List;
-//
-//@CrossOrigin
-//@RestController
-//@RequestMapping("/api/stores")
-//public class StoreController {
-//
-//    @Autowired
-//    private StoreRepository repository;
-//
-//    @Autowired
-//    private VehicleRepository vehicleRepository;
-//
-//    @GetMapping
-//    @ResponseBody
-//    public List<Store> getStores() {
-//        return repository.findAll();
-//    }
-//
-//
-//    @PostMapping
-//    public ResponseEntity<Store> createCar(@RequestBody Store newStore) {
-//        return new ResponseEntity<>(repository.save(newStore), HttpStatus.CREATED);
-//    }
-//
+package com.careerdevs.expressian.controllers;
+
+
+import com.careerdevs.expressian.entities.store.Store;
+import com.careerdevs.expressian.repositories.VehicleRepository;
+import com.careerdevs.expressian.repositories.StoreRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/api/stores")
+public class StoreController {
+
+    @Autowired
+    private StoreRepository repository;
+
+    @Autowired
+    private VehicleRepository vehicleRepository;
+
+    @GetMapping
+    @ResponseBody
+    public List<Store> getStores() {
+        return repository.findAll();
+    }
+
+
+    @PostMapping
+    public ResponseEntity<Store> createStore(@RequestBody Store newStore) {
+        return new ResponseEntity<>(repository.save(newStore), HttpStatus.CREATED);
+    }
+
 //    @PostMapping("/{carId}")
 //    public @ResponseBody Store newStore(@PathVariable Long carId, @RequestBody Store newStore) {
 //        Car car = carRepository.findById(carId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Car Not Found"));
 //        newStore.setCar(car);
 //        return repository.save(newStore);
 //    }
-//
-//    @DeleteMapping("/{id}")
-//    public ResponseEntity<String> destroyCar(@PathVariable Long id) {
-//        repository.deleteById(id);
-//        return new ResponseEntity<>("Deleted", HttpStatus.OK);
-//    }
-//
-//}
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> destroyCar(@PathVariable Long id) {
+        repository.deleteById(id);
+        return new ResponseEntity<>("Deleted", HttpStatus.OK);
+    }
+
+}
--- src/main/java/com/careerdevs/expressian/repositories/StoreRepository.java
+++ src/main/java/com/careerdevs/expressian/repositories/StoreRepository.java
@@ -1,9 +1,9 @@
-//package com.careerdevs.expressian.repositories;
-//
-//import com.careerdevs.expressian.entities.Store;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface StoreRepository extends JpaRepository<Store, Long> {
-//}
+package com.careerdevs.expressian.repositories;
+
+import com.careerdevs.expressian.entities.store.Store;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StoreRepository extends JpaRepository<Store, Long> {
+}
