4edae7f7a94a2ffb8d0b2317641989d8c3e8f622
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2022-01-09 13:05:09 -0500

    added Lombok dependency. Added more fields to the customer entity

--- src/main/java/com/careerdevs/expressian/entities/customer/Customer.java
+++ src/main/java/com/careerdevs/expressian/entities/customer/Customer.java
@@ -1,38 +1,38 @@
 package com.careerdevs.expressian.entities.customer;
 
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
 import javax.persistence.*;
 
 
 @Entity
+@NoArgsConstructor
+@ToString
+@Getter
+@Setter
 public class Customer {
+
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String customerName;
+    private  Long id;
+    private  String customerName;
+    private  String email;
+    private  String address;
+    private  String city;
+    private  String zip;
 
 
-    public Customer(){}
-
-    public Customer(String customerName) {
+    public Customer(String customerName, String email, String address, String city, String zip) {
         this.customerName = customerName;
-
+        this.email = email;
+        this.address = address;
+        this.city = city;
+        this.zip = zip;
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getCustomerName() {
-        return customerName;
-    }
-
-    public void setCustomerName(String customerName) {
-        this.customerName = customerName;
-    }
 }
